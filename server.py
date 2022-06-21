2a396fee8fcfe17adcbdde7c89f98ba888eeba6b
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2022-06-21 18:07:50 +0300

    Minor refactoring

--- server.py
+++ server.py
@@ -8,7 +8,6 @@ from flask import request
 from flask_login import LoginManager, login_user, current_user, login_required
 
 from config_loader import ConfigLoader
-from features.playlist_new_releases import PlaylistNewReleases
 from spotify_api import SpotifyAuth, SpotifyApi
 from users import User, UsersDb
 
@@ -46,15 +45,15 @@ def index():
 @app.route("/playlist_new_releases/select_playlist", methods=["GET"])
 @login_required
 def playlist_new_releases_select_playlist():
-    playlists = PlaylistNewReleases(current_user.access_token).get_playlists()
+    playlists = SpotifyApi(current_user.access_token).get_playlists()
     return render_template("playlist_selector.html", playlists=playlists, callback="/playlist_new_releases")
 
 
 @app.route("/playlist_new_releases", methods=["GET"])
 @login_required
-def playlist_new_releases_releases():
+def playlist_new_releases():
     playlist_id = request.args.get("id")
-    latest_dates, releases = PlaylistNewReleases(current_user.access_token).get_new_releases_for_playlist(playlist_id)
+    latest_dates, releases = SpotifyApi(current_user.access_token).get_new_releases_for_playlist(playlist_id)
     return render_template("playlist_new_releases.html", latest_dates=latest_dates, releases=releases)
 
 
