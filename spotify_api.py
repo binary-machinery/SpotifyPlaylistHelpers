7d3a4772db389bf9da8b987b08f3716c835c9a90
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2022-06-21 19:29:04 +0300

    A function to get paginated items

--- spotify_api.py
+++ spotify_api.py
@@ -72,9 +72,44 @@ class SpotifyApi:
     def get(self, endpoint, params=None):
         if params is None:
             params = {}
-        return requests.get(f"{self.api_url}{endpoint}?{urllib.parse.urlencode(params)}", headers={
-            "Authorization": f"Bearer {self.access_token}"
-        })
+        return requests.get(
+            f"{self.api_url}{endpoint}?{urllib.parse.urlencode(params)}",
+            headers={
+                "Authorization": f"Bearer {self.access_token}"
+            }
+        )
+
+    def get_paginated_items(self, endpoint, params, limit):
+        params["limit"] = limit
+        has_data = True
+        offset = 0
+        items = []
+        while has_data:
+            params["offset"] = offset
+            response = self.get(endpoint, params)
+            if not response.ok:
+                return items
+
+            json = response.json()
+            items.extend(json["items"])
+            has_data = False
+            if json["total"] > offset + limit:
+                has_data = True
+                offset += limit
+
+        return items
+
+    def post(self, endpoint, params=None, data=None):
+        if params is None:
+            params = {}
+        return requests.post(
+            f"{self.api_url}{endpoint}?{urllib.parse.urlencode(params)}",
+            headers={
+                "Authorization": f"Bearer {self.access_token}",
+                "Content-Type": "application/json"
+            },
+            data=data
+        )
 
     @staticmethod
     def _parse_album_date(album_json):
@@ -115,54 +150,42 @@ class SpotifyApi:
             json["owner"]["display_name"]
         )
 
-        has_data = True
+        items = self.get_paginated_items(
+            endpoint=f"/playlists/{playlist_id}/tracks",
+            params={
+                "fields": "total,items(track(id,name,album(id,name,release_date,release_date_precision,external_urls(spotify)),artists(id,name,external_urls(spotify))))"
+            },
+            limit=50
+        )
+
         tracks = []
-        limit = 50
-        offset = 0
-        while has_data:
-            response = self.get(
-                endpoint=f"/playlists/{playlist_id}/tracks",
-                params={
-                    "limit": limit,
-                    "offset": offset,
-                    "fields": "items(track(id,name,album(id,name,release_date,release_date_precision,external_urls(spotify)),artists(id,name,external_urls(spotify))))"
-                }
+        for track_meta_json in items:
+            track_json = track_meta_json["track"]
+
+            album_json = track_json["album"]
+            album = Album(
+                album_json["id"],
+                album_json["name"],
+                self._parse_album_date(album_json),
+                album_json["external_urls"]["spotify"]
             )
-            if not response.ok:
-                break
 
-            has_data = False
-            offset += limit
-
-            json = response.json()
-            for track_meta_json in json["items"]:
-                track_json = track_meta_json["track"]
-
-                album_json = track_json["album"]
-                album = Album(
-                    album_json["id"],
-                    album_json["name"],
-                    self._parse_album_date(album_json),
-                    album_json["external_urls"]["spotify"]
+            artists = []
+            for artist_json in track_json["artists"]:
+                artist = Artist(
+                    artist_json["id"],
+                    artist_json["name"],
+                    artist_json["external_urls"]["spotify"]
                 )
+                artists.append(artist)
 
-                artists = []
-                for artist_json in track_json["artists"]:
-                    artist = Artist(
-                        artist_json["id"],
-                        artist_json["name"],
-                        artist_json["external_urls"]["spotify"]
-                    )
-                    artists.append(artist)
-
-                track = Track(
-                    track_json["id"],
-                    track_json["name"],
-                    artists,
-                    album
-                )
-                tracks.append(track)
-                has_data = True
+            track = Track(
+                track_json["id"],
+                track_json["name"],
+                artists,
+                album
+            )
+            tracks.append(track)
 
         return playlist, tracks
 
@@ -180,40 +203,28 @@ class SpotifyApi:
         latest_dates = self.get_latest_song_by_artist(playlist_id)
         result = {}
         for artist, latest_song_date in latest_dates.items():
-            has_data = True
-            limit = 20
-            offset = 0
-            while has_data:
-                response = self.get(
-                    endpoint=f"/artists/{artist.id}/albums",
-                    params={
-                        "market": "FI",
-                        "include_groups": "album,single",
-                        "limit": limit,
-                        "offset": offset
-                    }
+            items = self.get_paginated_items(
+                endpoint=f"/artists/{artist.id}/albums",
+                params={
+                    "market": "FI",
+                    "include_groups": "album,single"
+                },
+                limit=20
+            )
+
+            for album_json in items:
+                release_date = self._parse_album_date(album_json)
+                if release_date <= latest_song_date:
+                    continue
+
+                album = Album(
+                    album_json["id"],
+                    album_json["name"],
+                    release_date,
+                    album_json["external_urls"]["spotify"]
                 )
-                if not response.ok:
-                    return result
-
-                has_data = False
-                offset += limit
-
-                json = response.json()
-                for album_json in json["items"]:
-                    release_date = self._parse_album_date(album_json)
-                    if release_date <= latest_song_date:
-                        continue
-
-                    album = Album(
-                        album_json["id"],
-                        album_json["name"],
-                        release_date,
-                        album_json["external_urls"]["spotify"]
-                    )
-                    if artist not in result:
-                        result[artist] = []
-                    result[artist].append(album)
-                    has_data = True
+                if artist not in result:
+                    result[artist] = []
+                result[artist].append(album)
 
         return latest_dates, result
