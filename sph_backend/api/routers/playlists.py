4524c62c5f2ab5cf53528ec8ea55bb6b55a0cff0
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2026-09-21 15:22:05 +0300

    Fix playlists endpoint url

--- sph_backend/api/routers/playlists.py
+++ sph_backend/api/routers/playlists.py
@@ -8,7 +8,7 @@ router = APIRouter(
 )
 
 
-@router.get("/playlists")
+@router.get("")
 async def get_playlists(spotify_service: SpotifyServiceDep):
     playlists = await spotify_service.get_playlists()
     return {"playlists": playlists}
